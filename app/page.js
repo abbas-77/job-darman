29318daffe8995f2f6114493218f6ad194b8a0fc
abbas-77 <abbas.azimii77@gmail.com>
2024-05-20 00:43:04 +0330

    create footer and fix this

--- app/page.js
+++ app/page.js
@@ -1,46 +1,51 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between home-bg-image">
-      <div className="flex min-h-screen w-full flex-col items-center justify-between p-16 bg-[#482BE770]">
-        <Navbar />
-        {/* landing page items */}
-        <div className="flex items-center justify-between w-full">
-          <div>
-            <FaChevronLeft size={25} color="lightgray" />
+    <main className="">
+      <div className="flex h-screen flex-col items-center justify-between home-bg-image">
+        <div className="flex min-h-screen w-full flex-col items-center justify-between p-16 bg-[#482BE770]">
+          <Navbar />
+          {/* landing page items */}
+          <div className="flex items-center justify-between w-full">
+            <div>
+              <FaChevronLeft size={25} color="lightgray" />
+            </div>
+            <div className="flex-col items-center justify-center w-[680px]">
+              <p className="text-white text-[32px] text-center font-semibold">
+                Startup 3
+              </p>
+              <p className="text-white text-[60px] text-center font-bold">
+                Forget About Code
+              </p>
+              <p className="text-white text-xl font-medium text-center">
+                Startup Framework gives you complete freedom over your creative
+                process — you don’t have to think about any technical aspects.
+                There are no limits and absolutely no coding.
+              </p>
+            </div>
+            <div>
+              <FaChevronRight size={25} color="white" />
+            </div>
           </div>
-          <div className="flex-col items-center justify-center w-[680px]">
-            <p className="text-white text-[32px] text-center font-semibold">
-              Startup 3
-            </p>
-            <p className="text-white text-[60px] text-center font-bold">
-              Forget About Code
-            </p>
-            <p className="text-white text-xl font-medium text-center">
-              Startup Framework gives you complete freedom over your creative
-              process — you don’t have to think about any technical aspects.
-              There are no limits and absolutely no coding.
-            </p>
+          {/* paginator */}
+          <div className="flex items-center gap-1.5">
+            <div className="border-2 rounded-full w-3 h-3 border-white bg-white"></div>
+            <div className="border-2 rounded-full w-3 h-3 border-white"></div>
+            <div className="border-2 rounded-full w-3 h-3 border-white"></div>
+            <div className="border-2 rounded-full w-3 h-3 border-white"></div>
+            <div className="border-2 rounded-full w-3 h-3 border-white"></div>
           </div>
-          <div>
-            <FaChevronRight size={25} color="white" />
-          </div>
+          {/* create an ecc button */}
+          <button className="w-[230px] h-[60px] bg-[#482BE7] rounded-full text-white font-medium text-lg">
+            Create an Account
+          </button>
         </div>
-        {/* paginator */}
-        <div className="flex items-center gap-1.5">
-          <div className="border-2 rounded-full w-3 h-3 border-white bg-white"></div>
-          <div className="border-2 rounded-full w-3 h-3 border-white"></div>
-          <div className="border-2 rounded-full w-3 h-3 border-white"></div>
-          <div className="border-2 rounded-full w-3 h-3 border-white"></div>
-          <div className="border-2 rounded-full w-3 h-3 border-white"></div>
-        </div>
-        {/* create an ecc button */}
-        <button className="w-[230px] h-[60px] bg-[#482BE7] rounded-full text-white font-medium text-lg">
-          Create an Account
-        </button>
       </div>
+      <div className="flex h-[120vh] flex-col items-center justify-between"></div>
+      <Footer />
     </main>
   );
 }
--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { FaFacebookF, FaGooglePlusG, FaTwitter } from "react-icons/fa";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-[#2F1893] h-[240px] py-10 px-10">
+      <div className="h-[50px] flex items-center justify-between">
+        <p className="text-[24px] text-white font-semibold">Startup 3</p>
+        <div>
+          <div className="flex items-center gap-4 text-white">
+            <p>Privacy Policy</p>
+            <p>Terms</p>
+            <p>
+              <FaTwitter size={25} />
+            </p>
+            <p>
+              <FaFacebookF size={25} />
+            </p>
+            <p>
+              <FaGooglePlusG size={35} />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="h-0.5 bg-gray-400 rounded-full my-10"></div>
+      <div className="h-[50px] flex items-center justify-between">
+        <div>
+          <div className="flex items-center gap-4 text-white">
+            <p>Tour</p>
+            <p>Features</p>
+            <p>Pricing Plans</p>
+            <p>Our Works</p>
+            <p>Brands</p>
+            <p>Contacts</p>
+          </div>
+        </div>
+        <p className="text-[16px] text-white">
+          © 2017 Designmodo. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
