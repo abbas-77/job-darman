e596d68ffa536898198d262502484939bec82f49
abbas-77 <abbas.azimii77@gmail.com>
2024-05-19 18:33:51 +0330

    add navbar items

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-center gap-5">
+      <div className="w-[60px] text-white">Home</div>
+      <div className="w-[60px] text-white">Feature</div>
+      <div className="w-[60px] text-white">Pricing</div>
+      <div className="w-[60px] text-white">Blog</div>
+      <div className="w-[60px] text-white">i</div>
+      <div className="w-[60px] text-white">Be</div>
+    </div>
+  );
 }
 
 export default Navbar;
