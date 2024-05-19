cc2cb72d1dbc6c633fdd96633d13b7a58b6e9ea2
abbas-77 <abbas.azimii77@gmail.com>
2024-05-19 19:07:05 +0330

    fix navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,25 @@
+"use client";
 import React from "react";
+import { FaDribbble } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
+  const pathname = usePathname();
   return (
     <div className="flex items-center justify-center gap-5">
-      <div className="w-[60px] text-white">Home</div>
+      <div
+        className={`w-[60px] ${
+          pathname === "/" ? "text-gray-300 font-medium" : "text-white"
+        }`}
+      >
+        Home
+      </div>
       <div className="w-[60px] text-white">Feature</div>
       <div className="w-[60px] text-white">Pricing</div>
       <div className="w-[60px] text-white">Blog</div>
-      <div className="w-[60px] text-white">i</div>
+      <div className="w-[50px] text-white">
+        <FaDribbble />
+      </div>
       <div className="w-[60px] text-white">Be</div>
     </div>
   );
