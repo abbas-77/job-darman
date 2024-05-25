1eaaab38355a8bea401aa641b4605633fad4ee0e
abbas-77 <abbas.azimii77@gmail.com>
2024-05-26 00:04:21 +0330

    add router to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,25 +2,55 @@
 import React from "react";
 import { FaDribbble } from "react-icons/fa";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
   const pathname = usePathname();
+  const router = useRouter();
   return (
     <div className="flex items-center justify-center gap-5">
       <div
-        className={`w-[60px] ${
+        onClick={() => router.push("/")}
+        className={`w-[60px] cursor-pointer hover:text-gray-300 ${
           pathname === "/" ? "text-gray-300 font-medium" : "text-white"
         }`}
       >
         Home
       </div>
-      <div className="w-[60px] text-white">Feature</div>
-      <div className="w-[60px] text-white">Pricing</div>
-      <div className="w-[60px] text-white">Blog</div>
+      <div
+        onClick={() => router.push("login")}
+        className={`w-[60px] cursor-pointer hover:text-gray-300 ${
+          pathname === "/login" ? "text-gray-300 font-medium" : "text-white"
+        }`}
+      >
+        Feature
+      </div>
+      <div
+        onClick={() => router.push("plans&pricing")}
+        className={`w-[60px] cursor-pointer hover:text-gray-300 ${
+          pathname === "/plans&pricing"
+            ? "text-gray-300 font-medium"
+            : "text-white"
+        }`}
+      >
+        Pricing
+      </div>
+      <div
+        onClick={() => router.push("pursche-budget")}
+        className={`w-[60px] cursor-pointer hover:text-gray-300 ${
+          pathname === "/pursche-budget"
+            ? "text-gray-300 font-medium"
+            : "text-white"
+        }`}
+      >
+        Blog
+      </div>
       <div className="w-[50px] text-white">
         <FaDribbble />
       </div>
-      <div className="w-[60px] text-white">Be</div>
+      <div className="w-[60px] cursor-pointer hover:text-gray-300 text-white">
+        Be
+      </div>
     </div>
   );
 }
